f1db35743befa243ef3f52bc4db7e8eb1d1b37f4
Kristian <jettkristian@gmail.com>
2020-09-19 10:40:08 -0700

    completed assignment #6 and bonus missions (except for second part of C)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,13 +23,12 @@ export class AppComponent {
       response.json().then(function(data) {
  
         let fetchedSatellites = data.satellites;
-        // TODO: loop over satellites
+        
         for(let i = 0; i < fetchedSatellites.length; i++){
           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(satellite);
         }
-        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+      
         this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));   
@@ -40,12 +39,17 @@ export class AppComponent {
       searchTerm = searchTerm.toLowerCase();
       for(let i=0; i < this.sourceList.length; i++) {
         let name = this.sourceList[i].name.toLowerCase();
+        let type = this.sourceList[i].type.toLowerCase();
+        let orbitType = this.sourceList[i].orbitType.toLowerCase();
         if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+        } else if (type.indexOf(searchTerm) >= 0){
+          matchingSatellites.push(this.sourceList[i]);
+        } else if (orbitType.indexOf(searchTerm) >= 0){
           matchingSatellites.push(this.sourceList[i]);
         }
       }
-    // assign this.displayList to be the array of matching satellites
-    // this will cause Angular to re-make the table, but now only containing matches
+   
     this.displayList = matchingSatellites;
   }
 
--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -8,11 +8,57 @@ import { Satellite } from '../satellite';
 })
 export class OrbitCountsComponent implements OnInit {
 
+  spaceDebrisCount: Number;
+  communicationCount: Number;
+  probeCount: Number;
+  positioningCount: Number;
+  spaceStationCount: Number;
+  telescopeCount: Number;
+
   @Input() satellites: Satellite[];
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit() {
+
+  }
+
+  ngDoCheck(){
+    this.checkCount();
+  }
+
+  checkCount() {
+    let debrisCount: Satellite[] = [];
+    let comCount: Satellite[] = [];
+    let probeCount: Satellite[] = [];
+    let positioningCount: Satellite[] = [];
+    let spaceStationCount: Satellite[] = [];
+    let telescopeCount: Satellite[] = [];
+
+    for (let i = 0; i < this.satellites.length; i++){
+      if (this.satellites[i].type === 'Space Debris'){
+        debrisCount.push(this.satellites[i]);
+      } else if (this.satellites[i].type === 'Communication'){
+        comCount.push(this.satellites[i]);
+      } else if (this.satellites[i].type === 'Probe'){
+        probeCount.push(this.satellites[i]);
+      } else if (this.satellites[i].type === 'Positioning'){
+        positioningCount.push(this.satellites[i]);
+      } else if (this.satellites[i].type === 'Space Station'){
+        spaceStationCount.push(this.satellites[i]);
+      } else if (this.satellites[i].type === 'Telescope'){
+        telescopeCount.push(this.satellites[i]);
+      }
+    }
+
+    this.spaceDebrisCount = debrisCount.length;
+    this.communicationCount = comCount.length;
+    this.probeCount = probeCount.length;
+    this.positioningCount = positioningCount.length;
+    this.spaceStationCount = spaceStationCount.length;
+    this.telescopeCount = telescopeCount.length;
   }
 
 }
